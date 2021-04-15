8e140efbf523ad158f4d4776243a5f374f586c44
Jesus Castro Olaya <programador3@email.cloudnets.co>
2021-04-15 17:00:13 -0500

    feat: primeros pasos para el modal

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,11 +1,18 @@
+import { Alert, Modal } from "bootstrap";
 import React from "react";
+import { ImgModal } from "./ImgModal";
+
 // Desestructuracion de props
 export const GifGridItem = ({ title, url }) => {
-  // console.log(title, url);
+  const handleClickImage = () => {
+    alert("hola");
+    // TODO: Un modal con la img-- Revisa el componente Imgmodal.js
+  };
   return (
     <div
-      style={{ color: "gray" }}
+      style={{ color: "gray", cursor: "pointer" }}
       className="card animate__animated animate__fadeIn mb-3"
+      onClick={handleClickImage}
     >
       <img src={url} alt={title} title={title} />
       <p style={{ color: "gray" }}>{title}</p>
---
+++ src/components/ImgModal.js
@@ -0,0 +1,7 @@
+import { Modal } from "bootstrap";
+
+import React from "react";
+
+export const ImgModal = () => {
+  return <Modal>Hola mundo!</Modal>;
+};
